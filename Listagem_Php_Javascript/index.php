f6222ab8fa7d38357ab9ceb3d6a715534695e1ad
omartins-zs <gabrielmatheus209@gmail.com>
2022-11-28 11:34:03 -0300

    Add Jquery e Mostrando usuarios na Tela

--- Listagem_Php_Javascript/index.php
+++ Listagem_Php_Javascript/index.php
@@ -6,15 +6,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listar com JavaScript</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 </head>
 
 <body>
-    <script>
-        (document).ready(function() {
-            $.post('listar_usuarios.php', function(retorna) {
+    <span id="conteudo"></span>
 
-            })
-        })
+    <script>
+        $(document).ready(function() {
+            $.post('listar_usuarios.php', function(retorna) {
+                //Subtitui o valor no seletor id="conteudo"
+                $("#conteudo").html(retorna);
+            });
+        });
     </script>
 </body>
 
