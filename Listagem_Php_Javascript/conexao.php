cd10e6cc3d939451b0bd3a29ea4b304390b01346
omartins-zs <gabrielmatheus209@gmail.com>
2022-11-28 10:57:46 -0300

    Adicionando arquivo de conexao com o Banco de Dados

---
+++ Listagem_Php_Javascript/conexao.php
@@ -0,0 +1,8 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "listagem_javascript_php";
+
+//Criar a conexao
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
